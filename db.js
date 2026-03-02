cb7c6a5d9710ba89adc1abacf9b46c0cf4d23983
kasireddimahitha <kasireddimahitha1729@gmail.com>
2026-03-02 16:06:27 +0530

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
-
-db.serialize(() => {
-  db.run("CREATE TABLE todos (id INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT, done INTEGER)");
-});
-
-module.exports = db;
